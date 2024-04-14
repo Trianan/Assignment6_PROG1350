7f2c9a27b4da06e605b6b000670f2e1ef5f8f84a
Tristin Manson <tristinmanson@gmail.com>
2024-04-14 11:12:35 -0400

    Add calculateNetPay function to CoolFeatureA.c. Added file-header comment as well, and style cleanup.

--- CoolFeatureA/CoolFeatureA.c
+++ CoolFeatureA/CoolFeatureA.c
@@ -1,4 +1,15 @@
+/* 
+    FILE: CoolFeatureA.c
+    AUTHOR: Tristin A. Manson
+    FIRST-VERSION: April 13th, 2024
+    DESCRIPTION:
+        This program holds data structures and functions for calculating
+        employee payroll.
+*/
+// ----------------------------------------------------------------------------
 #include <stdio.h>
+
+const double TAX_RATE = 0.205;
 
 typedef struct {
     unsigned int id;
@@ -7,18 +18,24 @@ typedef struct {
     double currentHours;
 } Worker;
 
+// ----------------------------------------------------------------------------
 double getGrossPay(Worker*);
+double calculateNetPay(double);
 void printWorker(Worker*);
 
+// ----------------------------------------------------------------------------
 int main(void) {
 
     Worker johnDoe = { 123456, "Carpenter", 26.75, 40.0 };
     printWorker(&johnDoe);
     printf("Gross pay for %u (%.1lf hours): ", johnDoe.id, johnDoe.currentHours);
-    printf("$%.2lf\n", getGrossPay(&johnDoe));
+    double johnGrossPay = getGrossPay(&johnDoe);
+    printf("$%.2lf\n", johnGrossPay);
+    printf("\tTake-home pay: $%.2lf\n", calculateNetPay(johnGrossPay));
     return 0;
 }
 
+// ----------------------------------------------------------------------------
 double getGrossPay(Worker* worker) {
     double hoursWorked = worker->currentHours;
     worker->currentHours = 0.0;
@@ -33,4 +50,8 @@ void printWorker(Worker* worker) {
         worker->hourlyWage,
         worker->currentHours
     );
+}
+
+double calculateNetPay(double grossPay) {
+    return grossPay - (grossPay * TAX_RATE);
 }
